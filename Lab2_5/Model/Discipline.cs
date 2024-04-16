f4879c3408a6d469759e1b64d7d144424a550dff
sh8m8n <118104171+sh8m8n@users.noreply.github.com>
2024-04-16 19:11:36 +0700

    Lab2_5

---
+++ Lab2_5/Model/Discipline.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Model
+{
+    public class Discipline
+    {
+        public string Title { get; set; }
+        public string TeacherName { get; set; }
+
+        public Difficulty Difficulty { get; set; }
+
+        public DateTime ExamDate { get; set; }
+        public DateTime PreparetionStart { get; set; }
+
+        public Discipline(string title, string teacherName, DateTime examDate, Difficulty difficulty)
+        {
+            Title = title;
+            TeacherName = teacherName;
+            Difficulty = difficulty;
+            PreparetionStart = examDate;
+            ExamDate = examDate;
+
+            switch(Difficulty)
+            {
+                case Difficulty.Normal:
+                    PreparetionStart.AddDays(-14);
+                    break;
+
+                case Difficulty.Hard:
+                    PreparetionStart.AddMonths(-1);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab2_5/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public ObservableCollection<Discipline> Disciplines = new ObservableCollection<Discipline>();
+
+        public Discipline SelectedDiscipline { get; set; }
+
+        public string Title { get; set; }
+        public string TeacherName { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public DateTime ExamDate { get; set; }
+
+        public RelayCommand AddCommand { get; set; }
+        public RelayCommand DeleteCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(AddDiscipline);
+            DeleteCommand = new RelayCommand(DeleteDiscipline, () => SelectedDiscipline != null);
+        }
+
+        private void AddDiscipline()
+        {
+            Disciplines.Add(new Discipline(Title, TeacherName, ExamDate, Difficulty));
+        }
+
+        private void DeleteDiscipline()
+        {
+            Disciplines.Remove(SelectedDiscipline);
+        }
+    }
+}
