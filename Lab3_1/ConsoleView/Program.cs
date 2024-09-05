d78b03ff4223ec22a3e19af428aeb5a078cd9d66
sh8m8n <118104171+sh8m8n@users.noreply.github.com>
2024-09-05 10:28:17 +0700

    Histogram

---
+++ Lab3_1/ConsoleView/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleHistogram;
+
+namespace ConsoleView
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Dictionary<string, int>()
+            {
+                {"газманов1", 10},
+                {"газманов2", 5 },
+                {"газманов3", 12 },
+                {"газманов4", 7 },
+                {"ааааaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 19 }
+            };
+
+            Histogram histogram = new Histogram();
+            histogram.Height = 20;
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine();
+            Console.WriteLine(histogram.GetHistogram(d));
+
+            Console.Read();
+
+        }
+    }
+}
